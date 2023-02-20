1cdc62f3002e71e1c5812b81081249d9f4e531c5
jauafelipe <jauafelipe345@gmail.com>
2023-02-20 10:55:26 -0300

    site bootstrap em manutençao

---
+++ assets/main.js
@@ -0,0 +1,78 @@
+const button1 = document.querySelector('#click1')
+const button2 = document.querySelector('#click2')
+const button3 = document.querySelector('#click3')
+const button4 = document.querySelector('#click4')
+const button5 = document.querySelector('#click5')
+const button6 = document.querySelector('#click6')
+const button7 = document.querySelector('#click7')
+
+
+
+button1.addEventListener('click', () => {
+    this.disable = true
+    button1.setAttribute('disabled', 'disabled')
+    const div = document.querySelector('#hyperClick1')
+    const p = document.createElement('p')
+    div.appendChild(p)
+    p.innerHTML = 'botao 1'
+
+})
+button2.addEventListener('click', () => {
+    this.disable = true
+    button2.setAttribute('disabled', 'disabled')
+    const div = document.querySelector('#hyperClick2')
+    const p = document.createElement('p')
+    div.appendChild(p)
+    p.innerHTML = 'botao 2'
+
+})
+button3.addEventListener('click', () => {
+    this.disable = true
+    button3.setAttribute('disabled', 'disabled')
+    const div = document.querySelector('#hyperClick3')
+    const p = document.createElement('p')
+    div.appendChild(p)
+    p.innerHTML = 'botao 3'
+
+})
+button4.addEventListener('click', () => {
+    this.disable = true
+    button4.setAttribute('disabled', 'disabled')
+    const div = document.querySelector('#hyperClick4')
+    const p = document.createElement('p')
+    div.appendChild(p)
+    p.innerHTML = 'botao 4'
+
+})
+button5.addEventListener('click', () => {
+    this.disable = true
+    button5.setAttribute('disabled', 'disabled')
+    const div = document.querySelector('#hyperClick5')
+    const p = document.createElement('p')
+    div.appendChild(p)
+    p.innerHTML = 'botao 5'
+
+})
+button6.addEventListener('click', () => {
+    this.disable = true
+    button6.setAttribute('disabled', 'disabled')
+    const div = document.querySelector('#hyperClick6')
+    const p = document.createElement('p')
+    div.appendChild(p)
+    p.innerHTML = 'botao 6'
+
+})
+
+button7.addEventListener('click', () => {
+    this.disable = true
+    button7.setAttribute('disabled', 'disabled')
+    const div = document.querySelector('#hyperClick7')
+    const p = document.createElement('p')
+    div.appendChild(p)
+    p.innerHTML = 'botao 7'
+
+})
+
+
+
+
